e649a1e1e04038c13859d13d3d674eaa79328d1d
kojiro0208 <h.wadsworth02@gmail.com>
2023-08-26 16:41:22 +0900

    変更

--- app/app.py
+++ app/app.py
@@ -2,4 +2,4 @@ import json
 
 
 def lambda_handler(event, context):
-    return {"statusCode": 200, "body": json.dumps("Hello from Update Lambda!")}
+    return {"statusCode": 200, "body": json.dumps("Hello from Update Lambda!!")}
